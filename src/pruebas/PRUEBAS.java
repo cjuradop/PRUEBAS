8413793e8a59feebe7d61b4f23fdb179dbf6275a
cjura <cjura@LAPTOP-0R6U99Q8.home>
2020-11-09 21:12:39 +0100

    Este es el segundo commit de prueba

--- src/pruebas/PRUEBAS.java
+++ src/pruebas/PRUEBAS.java
@@ -4,6 +4,10 @@ public class PRUEBAS {
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        
+
+        System.out.println("PARSE");
+        System.out.println("PARSE");
     }
 
 }
